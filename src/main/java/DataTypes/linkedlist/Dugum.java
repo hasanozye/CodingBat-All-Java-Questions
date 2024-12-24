04e4beb5660888b5fc01a75ccefa2d0f3347c6e7
hasan.ozyer <hasanozyer06@gmail.com>
2024-12-24 19:16:22 +0300

    Added Dugum class for linked list implementation.

---
+++ src/main/java/DataTypes/linkedlist/Dugum.java
@@ -0,0 +1,19 @@
+package DataTypes.linkedlist;
+
+public class Dugum {
+
+    public Object oge;
+    public Dugum link;
+
+    public Dugum(){
+        this.oge = null;
+        this.link = null;
+    }
+
+    public Dugum(Object veri){
+        this.oge = veri;
+        this.link = null;
+    }
+
+
+}
